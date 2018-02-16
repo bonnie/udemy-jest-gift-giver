6f3901cf6a9865f44a909d5a1d00591d399bda2e
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-02-16 08:51:42 -0800

    Update for jest/enzyme. Create app component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,5 +7,7 @@ const app = shallow(<App />)
 
 // jest uses global "it" and "expect"
 it('renders correctly', () => {
+  // jest snapshots keep a recorded history of react component
+  // it updates the snapshot every time the component changes
   expect(app).toMatchSnapshot()
 })
