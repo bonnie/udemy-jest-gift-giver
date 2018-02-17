5ea83bb839eaac5b0a45dd9a3c37e38a6db0ae75
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-02-16 16:00:14 -0800

    WIP: start adding tests and code to add gift

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { gifts: [] }
+  }
+
+  addGift = () => {
+    const { gifts } = this.state
+
+    // find max id
+    const maxId = Math.max(...gifts.map(gift => gift.id)) || 0
+    gifts.push({ id: maxId + 1 })
+  }
+
   render () {
     return (
-      <div></div>
+      <div>
+        <h2>Gift Giver</h2>
+        <Button className='btn-add'>Add Gift</Button>
+      </div>
     )
   }
 }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,6 +8,20 @@ const app = shallow(<App />)
 // jest uses global "it" and "expect"
 it('renders correctly', () => {
   // jest snapshots keep a recorded history of react component
-  // it updates the snapshot every time the component changes
+  // the test runner allows for updating the snapshot every time the component changes
+  // basically the snapshot is a cannonical description of how the component should look
   expect(app).toMatchSnapshot()
+})
+
+it('initializes the `state` with an empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([])
+})
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+  // first find the addGift button, then test that the state updates
+  // .find can find by tag or classname (start with period)
+  // enzyme provides .simulate
+  app.find('.btn-add').simulate('click')
+
+  expect(app.state().gifts.to.deeply.equal([{ id: 1 }]))
 })
